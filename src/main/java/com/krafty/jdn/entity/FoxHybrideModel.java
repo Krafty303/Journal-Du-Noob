3461e8d4159b273c723bd78755df87545dac00ad
Krafty303 <115349008+Krafty303@users.noreply.github.com>
2023-01-22 17:22:04 +0100

    Un build de IntelliJ

--- src/main/java/com/krafty/jdn/entity/FoxHybrideModel.java
+++ src/main/java/com/krafty/jdn/entity/FoxHybrideModel.java
@@ -1,11 +1,16 @@
-// Made with Blockbench 4.6.0
+package com.krafty.jdn.entity;// Made with Blockbench 4.6.0
 // Exported for Minecraft version 1.17 or later with Mojang mappings
 // Paste this class into your mod and generate all required imports
 
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 
 public class FoxHybrideModel<T extends Entity> extends EntityModel<T> {
