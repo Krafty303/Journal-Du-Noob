a26b03c5f76845160612df96e111617f96f95b02
Krafty303 <115349008+Krafty303@users.noreply.github.com>
2023-01-23 09:52:19 +0100

    Update JournalDuNoobMod.java

--- src/main/java/com/krafty/jdn/JournalDuNoobMod.java
+++ src/main/java/com/krafty/jdn/JournalDuNoobMod.java
@@ -1,24 +1,17 @@
 package com.krafty.jdn;
 
+import com.krafty.jdn.init.BlockInit;
+import com.krafty.jdn.init.EntityInit;
+import com.krafty.jdn.init.ItemInit;
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -29,19 +22,12 @@ import org.slf4j.Logger;
 @Mod(JournalDuNoobMod.MODID)
 public class JournalDuNoobMod {
     public static final String MODID = "jdn";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-
-    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Block> BAICLAIR_BUSH_BLOCK = BLOCKS.register("baiclair_bush_block", () -> new Block(BlockBehaviour.Properties.of(Material.PLANT)));
-    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Item> BAICLAIR_ITEM = ITEMS.register("baiclair_item", () -> new BlockItem(BAICLAIR_BUSH_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
 
     public JournalDuNoobMod() {
-        IEventBus
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        BlockInit.BLOCKS.register(bus);
+        ItemInit.ITEMS.register(bus);
+        EntityInit.ENTITIES.register(bus);
     }
 }
